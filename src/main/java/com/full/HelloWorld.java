3ed2d55c8d8bde5658973cfe80f5159f75abb909
Theeran <theeran@FULL157-AIR.FULL.Creative>
2017-04-28 13:56:11 +0530

    initial commit

---
+++ src/main/java/com/full/HelloWorld.java
@@ -0,0 +1,24 @@
+package com.full;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class HelloWorld extends HttpServlet
+{
+
+  @Override
+  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException 
+  {
+    PrintWriter out = resp.getWriter();
+    out.println("Hello hai from the gradle!!!!!!!");
+    
+   
+    
+  
+  }
+}
